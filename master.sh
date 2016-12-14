aacf38f6dad6d2862efcb7a045cc689677de9b64
pizzic <juven@stanfordalumni.org>
2016-12-14 03:27:58 +0000

    Changed directory that master.csv gets put and uploaded into HDFS from.

--- master.sh
+++ master.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Load the master data set into HDFS.
-tail -n +2 Master_Data_Set.csv > ./master.csv
+tail -n +2 Master_Data_Set.csv > ./data/master.csv
 
 hdfs dfs -mkdir /user/w205/data/master
 hdfs dfs -rm /user/w205/data/master/*
-hdfs dfs -put master.csv /user/w205/data/master/
+hdfs dfs -put data/master.csv /user/w205/data/master/
 
 # Load the DDL so Hive can parse the file.
 hive -f sql/master_ddl.sql
