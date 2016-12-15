05a1e47280b7208b282dadab47d0b1ba26747a0f
pizzic <juven@stanfordalumni.org>
2016-12-15 07:49:09 +0000

    Calling Hospital Safety Grade script multiple times.

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@
 # Download the Yelp data.
 python3.4 yelp_pull.py
 
-#Download the Hospital Safety Score data.
-python3 Hospital_Safety_Grade.py
+# Download the Hospital Safety Score data.
+# Have to do this in multiple parts because the script can't do all the states at once.
+python3 Hospital_Safety_Grade.py 1
+python3 Hospital_Safety_Grade.py 2
+python3 Hospital_Safety_Grade.py 3
+python3 Hospital_Safety_Grade.py 4
+python3 Hospital_Safety_Grade.py 5
 
 # Merge the data sets.
 python3 data_merge.py
